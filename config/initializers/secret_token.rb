23d18807af39224f814b92f872327447ffd04511
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-04-04 18:47:53 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exchangeagram::Application.config.secret_key_base = 'aed53248121710188fba99f02e5b8b9b29c49d14f2979e77c2c4663606c1067fe47f0dc7156623b47a3ebedf33c9b6e97e776205b4b8c622972a8ad26f42f9d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exchangeagram::Application.config.session_store :cookie_store, key: '_exchangeagram_session'
